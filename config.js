300d28a2417d427336a056689a66e4ae26f3643e
Lance Bierke <lancebierke@gmail.com>
2021-09-17 13:28:49 -0700

    Passport Facebook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
   secretKey: "12345-67890-09876-54321",
   mongoUrl: "mongodb://localhost:27017/nucampsite",
+  facebook: {
+    clientId: "607570156913760",
+    clientSecret: "0abc93dbee6b9beaa9f5a72bf4faa821",
+  },
 };
